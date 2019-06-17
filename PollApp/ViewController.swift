a8e51df2f7f8d29d9f45533b5812857c4e1e5837
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-06-17 12:14:20 -0400

    Initial Commit

---
+++ PollApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 6/17/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
