63bc2ba9e859b3e16146de98e68983d8cf5e9926
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-06-18 16:25:01 -0400

    finished login

---
+++ PollApp/VerifyViewController.swift
@@ -0,0 +1,44 @@
+//
+//  VerifyViewController.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 6/18/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class VerifyViewController: UIViewController {
+    @IBOutlet weak var code: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Enter Your Verification Code"
+    }
+    
+    @IBAction func submitCode(sender: UIButton) {
+        let verificationCode = String(code.text!)
+        let verificationID = String(UserDefaults.standard.string(forKey: "authVerificationID")!)
+
+        let credential = PhoneAuthProvider.provider().credential(
+            withVerificationID: verificationID,
+            verificationCode: verificationCode)
+        
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                self.showMessagePrompt(message: error.localizedDescription)
+                return
+            }
+            self.showMessagePrompt(message: authResult?.user.phoneNumber ?? "no number")
+        }
+    }
+    
+    func showMessagePrompt(message: String){
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Button", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
--- PollApp/ViewController.swift
+++ PollApp/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "Enter Your Phone Number"
+    }
+    
+    @IBAction func sendText(sender: UIButton) {
+        var number = String(field.text!).filter("01234567890".contains)
+        if number.count == 10 {
+            number = "1" + number
+        }
+        number = "+" + number
+        
+        PhoneAuthProvider.provider().verifyPhoneNumber(number, uiDelegate: nil) { (verificationID, error) in
+            if let error = error {
+                self.showMessagePrompt(message: error.localizedDescription)
+                return
+            }
+            UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+            
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            
+            let resultViewController = storyBoard.instantiateViewController(withIdentifier: "VerifyViewController") as! VerifyViewController
+            
+            self.present(resultViewController, animated:true, completion:nil)
+        }
+    }
+    
+    func showMessagePrompt(message: String){
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Button", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 
 
