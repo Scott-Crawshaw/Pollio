75d76005d94058e7940209290912fb12e404ce4e
BenTheBen <benstewartbs@gmail.com>
2019-06-20 16:10:31 -0400

    UI Updates! :)

--- PollApp/ViewController.swift
+++ PollApp/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var field: UITextField!
     @IBOutlet weak var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         label.text = "Enter Your Phone Number"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
     }
     
     @IBAction func sendText(sender: UIButton) {
@@ -26,6 +32,11 @@ class ViewController: UIViewController {
             number = "1" + number
         }
         number = "+" + number
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "VerifyViewController") as! VerifyViewController
+        
+        self.present(resultViewController, animated:true, completion:nil)
         
         PhoneAuthProvider.provider().verifyPhoneNumber(number, uiDelegate: nil) { (verificationID, error) in
             if let error = error {
@@ -34,11 +45,7 @@ class ViewController: UIViewController {
             }
             UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
             
-            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-            
-            let resultViewController = storyBoard.instantiateViewController(withIdentifier: "VerifyViewController") as! VerifyViewController
-            
-            self.present(resultViewController, animated:true, completion:nil)
+
         }
     }
     
@@ -46,6 +53,18 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Button", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 212/255.0, green: 119/255.0, blue: 230/255.0, alpha: 0.9).cgColor
+        let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop,colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 
 
