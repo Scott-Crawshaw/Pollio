7911882379144371313c341de93d8ec2870ac89d
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-06-21 12:33:13 -0400

    pod install and created api files

---
+++ PollApp/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 6/21/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+var ref: DatabaseReference!
+
+static func getUserByUID(UID: String){
+    
+}
---
+++ PollApp/user.swift
@@ -0,0 +1,14 @@
+//
+//  user.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 6/21/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    var name: String
+    
+}
