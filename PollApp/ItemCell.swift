60117c39370e123f9256b257d574c5407ff3fb0c
BenTheBen <benstewartbs@gmail.com>
2019-07-09 16:00:28 -0400

    ContactView

---
+++ PollApp/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  PollApp
+//
+//  Created by Ben Stewart on 7/9/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
