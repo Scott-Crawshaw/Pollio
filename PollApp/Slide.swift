12372fcc3970472436a11a29dee9a389f1793ecc
BenTheBen <benstewartbs@gmail.com>
2019-07-11 13:02:32 -0400

    Contacts pt 2

--- PollApp/Slide.swift
+++ PollApp/Slide.swift
@@ -9,22 +9,36 @@
 import UIKit
 import Contacts
 
-class Slide: UIViewController {
+class Slide: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let key = numArray[indexPath.item]
+        cell.textLabel?.text = (cDict[key])
+        
+        return cell
+    }
+    
 
     @IBOutlet var contactSwitch: UISwitch!
     @IBOutlet var loadPrompt: UIActivityIndicatorView!
-    @IBOutlet var tabView: UITableView!
-    var tabDataSource: ContactTableController!
+    @IBOutlet weak var tabView: UITableView!
+    
     
     var cDict: [String: String] = [:]
     var numArray: [String] = []
+    
+    override func viewDidLoad() {
+    
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         setGradientBackground()
-        tabView.isHidden = true
-        tabView.dataSource = tabDataSource
-        tabView.delegate = tabDataSource
-
         super.viewWillAppear(animated)
     }
     
@@ -81,11 +95,12 @@ class Slide: UIViewController {
         //this is where you fill the table view. the format of the data is as follows
         //[[number:+16176108187, userID:djakdn23rj2k3nds], [number:+17815550111, userID:djakdn23rj2k3nds]]
         for c in contacts{
-            numArray.append(c["number:"] as! String)
+           numArray.append(c["number"] as! String)
         }
         UserDefaults.standard.set(numArray, forKey: "numberArray")
         UserDefaults.standard.set(cDict, forKey: "contactDictionary")
-        tabView.isHidden = false
+        cDict = UserDefaults.standard.dictionary(forKey: "contactDictionary") as! [String : String]
+        numArray = UserDefaults.standard.array(forKey: "numberArray") as! [String]
         //pulling up the next VC
 
     }
@@ -95,6 +110,7 @@ class Slide: UIViewController {
     func setGradientBackground() {
         let colorTop =  UIColor(red: 212/255.0, green: 119/255.0, blue: 230/255.0, alpha: 0.9).cgColor
         let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 1.0).cgColor
+       
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop,colorBottom]
