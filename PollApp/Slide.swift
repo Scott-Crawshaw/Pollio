697ba7c0a6f43b5c826ff9a396a0de375816661d
BenTheBen <benstewartbs@gmail.com>
2019-07-16 16:39:32 -0400

    good day today champ

--- PollApp/Slide.swift
+++ PollApp/Slide.swift
@@ -163,6 +163,10 @@ class Slide: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     func setGradientBackground() {
         let colorTop =  UIColor(red: 212/255.0, green: 119/255.0, blue: 230/255.0, alpha: 0.9).cgColor
         let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 1.0).cgColor
