82fdf3278ccde419bf07cb3f5961c247ccea824c
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-07-18 12:30:50 -0400

    commit.io

---
+++ PollApp/PollTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PollTableViewCell.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 7/18/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import UIKit
+
+class PollTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
