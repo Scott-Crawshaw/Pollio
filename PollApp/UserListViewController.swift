d64515fe0c93b67b2c5809a0b0daf0454335a711
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-08-01 14:54:21 -0400

    stew

--- PollApp/UserListViewController.swift
+++ PollApp/UserListViewController.swift
@@ -13,12 +13,15 @@ class UserListViewController: UITableViewController {
     var infoRef : String = ""
     var arrName : String = ""
     var titleText : String = ""
-    var data : [[String : Any]] = []
+    var tableData : [[String : Any]] = []
     
     @IBOutlet var navTitle: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,8 +38,7 @@ class UserListViewController: UITableViewController {
     
     func populateData(result: [[String : Any]]?){
         if result != nil{
-            data = result!
-            print(data)
+            tableData = result!
             self.tableView.reloadData()
         }
         else{
@@ -48,21 +50,20 @@ class UserListViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        print(data.count)
-        return data.count
+        return tableData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserListViewCell
         print("got cell")
-        cell.username_label.text = data[indexPath.row]["username"] as? String ?? "error"
-        cell.name_label.text = data[indexPath.row]["name"] as? String ?? "error"
-        cell.uid = data[indexPath.row]["user"] as? String ?? ""
+        cell.username_label.text = tableData[indexPath.row]["username"] as? String ?? "error"
+        cell.name_label.text = tableData[indexPath.row]["name"] as? String ?? "error"
+        cell.uid = tableData[indexPath.row]["user"] as? String ?? ""
 
         return cell
     }
@@ -70,7 +71,7 @@ class UserListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let newViewController = storyBoard.instantiateViewController(withIdentifier: "yourProfile") as! YourProfileView
-        newViewController.uid = data[indexPath.row]["user"]! as! String
+        newViewController.uid = tableData[indexPath.row]["user"]! as! String
         self.present(newViewController, animated: true, completion: nil)
     }
     
