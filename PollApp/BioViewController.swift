965dfd74b6a9cea5cab1a932565a6da17249e032
BenTheBen <benstewartbs@gmail.com>
2019-08-15 22:15:45 -0400

    I am so done with this bio page why does nothing look good fml.

--- PollApp/BioViewController.swift
+++ PollApp/BioViewController.swift
@@ -17,17 +17,21 @@ class BioViewController: UIViewController, UITextViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let grey = UIColor(red: 67/255.0, green: 67/255.0, blue: 67/255.0, alpha: 1.0)
+        let polliopurp = UIColor(red: 198/255.0, green: 76/255.0, blue: 222/255.0, alpha: 1.0)
+
+
         
         //setting up Text Field
         bioTextView.delegate = self
-        let polliopurp = UIColor(red: 198/255.0, green: 76/255.0, blue: 222/255.0, alpha: 1.0)
         bioTextView.layer.borderWidth = 3
-        bioTextView.layer.borderColor = polliopurp .cgColor
+        bioTextView.layer.borderColor = UIColor .darkGray .cgColor
         
         //setting up Button
         
-        submitButton.layer.borderWidth = 3
-        submitButton.layer.borderColor = UIColor .darkGray .cgColor
+
+        //submitButton.layer.borderWidth = 3
+        //submitButton.layer.borderColor = UIColor .darkGray .cgColor
         submitButton.layer.cornerRadius = 3.5
         
         
