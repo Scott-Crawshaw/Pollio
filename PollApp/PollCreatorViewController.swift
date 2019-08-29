a93c3a75a89e06a128b83d65cc85dfc9fb614939
BenTheBen <benstewartbs@gmail.com>
2019-08-29 19:21:52 -0400

    good lord this merge boutta smack my cheeks

--- PollApp/PollCreatorViewController.swift
+++ PollApp/PollCreatorViewController.swift
@@ -50,7 +50,7 @@ class PollCreatorViewController: UIViewController, UITextViewDelegate {
     
     func setGradientBackground() {
         let colorTop =  UIColor(red: 212/255.0, green: 119/255.0, blue: 230/255.0, alpha: 0.9).cgColor
-        let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 0.9).cgColor
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop,colorBottom]
