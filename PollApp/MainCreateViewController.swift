687b4495160c019b4bfcb57c8a0cf962730a4403
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-09-04 15:13:28 -0400

    everything works on iphone 5s

---
+++ PollApp/MainCreateViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainCreateViewController.swift
+//  PollApp
+//
+//  Created by Scott Crawshaw on 9/4/19.
+//  Copyright © 2019 Crawtech. All rights reserved.
+//
+
+import UIKit
+
+class MainCreateViewController: UIViewController {
+
+    @IBOutlet weak var scrollview: UIScrollView!
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGradientBackground()
+        //scrollview.contentSize = CGSize(width: self.contentView.frame.width, height: self.contentView.frame.height+1000)
+
+        // Do any additional setup after loading the view.
+    }
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 212/255.0, green: 119/255.0, blue: 230/255.0, alpha: 0.9).cgColor
+        let colorBottom = UIColor(red: 161/255.0, green: 84/255.0, blue: 194/255.0, alpha: 0.9).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop,colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
